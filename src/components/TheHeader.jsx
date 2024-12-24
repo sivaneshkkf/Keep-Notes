50801dc1dfeea017af1f2f5488ec94b15308d1a6
Sivaneshkkf <sivaneshkkf@gmail.com>
2024-12-24 16:59:34 +0530

    fix: header

--- src/components/TheHeader.jsx
+++ src/components/TheHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TheHeader = () => {
   return (
     <div className='bg-white py-1 w-screen'>
-        <h1 className="text-text font-bold text-lg text-center">Keep Notes</h1>
+        <h1 className="text-text font-bold text-lg text-center">KEEP NOTES</h1>
     </div>
   )
 }
